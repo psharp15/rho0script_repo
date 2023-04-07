2d07efccaae36f26ca2ee336606d56dbabfaacd1
psharp15 <psharp15@gwu.edu>
2023-04-07 11:55:09 -0400

    made the run number(p) the 3rd arg

--- gcf/run_pp2p_gcf.sh
+++ gcf/run_pp2p_gcf.sh
@@ -12,10 +12,12 @@ else
 		echo "Running potential $1 over target $2"
 fi
 
+p=$3
+
 		
 for p in 0 #1 2 3 4
 do
-		root -l -b -q /work/halld2/home/psharp/analysis/rho0script_repo/gcf/pp2p_gcf.c\(\"/volatile/halld/home/psharp/simulation/gcf_sim_slurm/$1_$2/genoutput_${p}.root\",\"/work/halld2/home/psharp/analysis/simulation/gcf_sim/test/$1_$2/rho0_${p}_analyzed.root\"\)
+		root -l -b -q /work/halld2/home/psharp/analysis/rho0script_repo/gcf/pp2p_gcf.c\(\"/volatile/halld/home/psharp/simulation/gcf_sim_slurm/$1_$2/genoutput_${p}.root\",\"/work/halld2/home/psharp/analysis/simulation/gcf_sim/tesst/$1_$2/rho0_${p}_analyzed.root\"\)
 done
 
 #hadd /work/halld2/home/psharp/gcf_sim/AV4_C12/histsall.root /work/halld2/home/psharp/gcf_sim/AV4_C12/rho_*_analyzed.root
