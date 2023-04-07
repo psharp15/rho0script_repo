961139536992735e45d21d9638e74297499255ee
psharp15 <psharp15@gwu.edu>
2023-04-07 10:24:16 -0400

    updated with correct paths

--- gcf/run_pp2p_gcf.sh
+++ gcf/run_pp2p_gcf.sh
@@ -15,7 +15,7 @@ fi
 		
 for p in 0 #1 2 3 4
 do
-		root -l -b -q /work/halld2/home/psharp/rho0analysis/rhoscript_repo/ppvsp_rho0.c\(\"/volatile/halld/home/psharp/gcf_sim_slurm/$1_$2/genoutput_${p}.root\",\"/work/halld2/home/psharp/gcf_sim/$1_$2/rho0_${p}_analyzed.root\",\"/work/halld2/home/psharp/gcf_sim/$1_$2/rho0_${p}_pp2p.txt\"\)
+		root -l -b -q /work/halld2/home/psharp/analysis/rho0script_repo/gcf/pp2p_gcf.c\(\"/volatile/halld/home/psharp/gcf_sim_slurm/$1_$2/genoutput_${p}.root\",\"/work/halld2/home/psharp/analysis/simulation/gcf_sim/test/$1_$2/rho0_${p}_analyzed.root\"\)
 done
 
 #hadd /work/halld2/home/psharp/gcf_sim/AV4_C12/histsall.root /work/halld2/home/psharp/gcf_sim/AV4_C12/rho_*_analyzed.root
