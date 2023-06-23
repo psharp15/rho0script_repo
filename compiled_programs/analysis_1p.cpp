0e484b6f4b4e778789e2250bc28721576cc4ad29
Phoebe <phoebe@PhoebesComputer.local>
2023-06-22 22:21:12 -0400

    it now takes inputs! Huzzah!

--- compiled_programs/analysis_1p.cpp
+++ compiled_programs/analysis_1p.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include "TFile.h"
+
 
 using namespace std ;
 
 int main(int argc, char** argv){
+	if (argc!=3){
+	cerr << "You must add some inputs here!";
+	return -1;
+	}
+	TFile *infile= new TFile(argv[1]);
+	TFile *outfile= new TFile(argv[2],"RECREATE");
 	cout << "Hello world!\n";
 	return 0;
 }
