512525495a21e8ee553a87aedd5018320f8d9303
psharp15 <psharp15@gwu.edu>
2023-07-13 11:29:52 -0400

    cleaned up and updated paths

--- gcf/run_pp2p_gcf.sh
+++ gcf/run_pp2p_gcf.sh
@@ -1,28 +1,11 @@
 #$!/bin/bash
-#echo "Chose a potential: AV18, N2L0, or AV4. And then a target: C12, He4, D2"
-
-
-#if [ $# -eq 0 ];
-#then
-#		echo "$0: Missing arguments"
-#		exit 1
-#elif [$# -gt 2];
-#then
-#		echo "$0: Too many arguments: $@"
-#		exit 1
-#else
-#		echo "Running potential $1 over target $2"
-#fi
 
 potential=$1
 target=$2
 p=$3
 		
-#for p in {0..9}
-#do
-		root -l -b -q /work/halld2/home/psharp/rho0script_repo/gcf/pp2p_gcf.c\(\"/volatile/halld/home/psharp/simulation/gcf_raw/8GeV_gcf_raw/$1_$2/genoutput_${p}.root\",\"/work/halld2/home/psharp/simulation/output/gcf_raw/8GeV/$1_$2/rho0_${p}_analyzed.root\"\)
-#done
+
+root -l -b -q /work/halld2/home/psharp/rho0script_repo/gcf/pp2p_gcf.c\(\"/volatile/halld/home/psharp/simulation/gcf_raw/8GeV/$1_$2/genoutput_${p}.root\",\"/work/halld2/home/psharp/simulation/output/gcf_raw/8GeV/$1_$2/rho0_${p}_analyzed.root\"\)
 
 echo "done with ${target}, ${potential}, number ${p} "
 
-#hadd /work/halld2/home/psharp/simulation/output/gcf_raw/$1_$2/pp2p_output_$1_$2.root /work/halld2/home/psharp/simulation/output/gcf_raw/$1_$2/rho_*_analyzed.root
