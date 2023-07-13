1c52636d8a63aae02946845252fb0b85f8e6fafe
psharp15 <psharp15@gwu.edu>
2023-07-13 13:46:15 -0400

    cleaned up

--- gcf/run_gcf.sh
+++ gcf/run_gcf.sh
@@ -1,23 +1,13 @@
 #read run "potential" "target"
-if [ $# < 2 ]
-then
-    echo "You must provide an argument!"
-    exit -1
-fi
+#if [ $# < 2 ]
+#then
+#    echo "You must provide an argument!"
+#    exit -1
+#fi
 
 potential=$1
 target=$2
 run=$3
-
-#if [target = "C12"]
-#then Z=6 && N=6
-
-#elif [target = "He4"]
-#then Z=2 && N=2
-
-#else [target = "D2"]
-#then Z=1 && N=1
-#fi 
 
 case $1 in
         AV18)
@@ -56,17 +46,7 @@ case $2 in
 esac
 echo "Inputs are valid"
 
-#if [$3 = C12]
-#then Z=6 #N=6
-
-#elif [$3 = He4]
-#then Z=2 #N=2
-
-#else [$3 = D2]
-#Z=1 #N=1
-#fi 
-
-program=/w/halld-scshelf2101/halld2/home/psharp/simulation/GCF_Generator_Suite/build/src/programs/genPhoto/genPhoto
+program=/work/halld2/home/psharp/simulation/GCF_Generator_Suite/build/src/programs/genPhoto/genPhoto
 
 outdir=/volatile/halld/home/psharp/simulation/gcf_raw/8GeV/${potential}_${target}
 outfile=${outdir}/genoutput_${run}.root
